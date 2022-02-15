fc94a880356fea91f6292770e0aa8968e964a62d
Somad <iretibe@users.noreply.github.com>
2022-02-15 14:55:33 +0000

    first commit

---
+++ FriendZone.Models/FriendModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FriendZone.Models
+{
+    public class FriendModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
